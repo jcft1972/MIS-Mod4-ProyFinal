947f0e6ea8c2b1098d14de85821095f4e6d5a316
jcft1972 <33870215+jcft1972@users.noreply.github.com>
2018-04-12 16:31:00 -0400

    Crea la estructura REST

---
+++ backend/urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primenumbers/PrimeNumbersResources.java
@@ -0,0 +1,4 @@
+package com.alenasoft.urbanager.resources.primenumbers;
+
+public class PrimeNumbersResources {
+}
---
+++ backend/urbanager-application/src/main/java/com/alenasoft/urbanager/resources/primenumbers/service/PrimeNumbersService.java
@@ -0,0 +1,4 @@
+package com.alenasoft.urbanager.resources.primenumbers.service;
+
+public interface PrimeNumbersService {
+}
